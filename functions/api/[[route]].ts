13fc4bd255ca1b39b33a396a788ad5f6c29ff6dd
Riley Paul <rileypaul96@gmail.com>
2024-05-16 14:17:46 -0600

    get dev server working

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,26 +1,32 @@
-import { zValidator } from '@hono/zod-validator'
-import { Hono } from 'hono'
-import { handle } from 'hono/cloudflare-pages'
-import { z } from 'zod'
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from "hono";
+import { handle } from "hono/cloudflare-pages";
+import { logger } from "hono/logger";
+import { z } from "zod";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
+
+app.use(logger());
 
 const route = app.get(
-  '/hello',
+  "/hello",
   zValidator(
-    'query',
+    "query",
     z.object({
-      name: z.string()
+      name: z.string(),
     })
   ),
   (c) => {
-    const { name } = c.req.valid('query')
+    const { name } = c.req.valid("query");
     return c.json({
-      message: `Hello fasdfasdf ${name}!`
-    })
+      message: `Hello fasdfasdf ${name}!`,
+    });
   }
-)
+);
 
-export type AppType = typeof route
-
-export const onRequest = handle(app)
+export type AppType = typeof route;
+export default {
+  port: 5000,
+  fetch: app.fetch,
+};
+export const onRequest = handle(app);
